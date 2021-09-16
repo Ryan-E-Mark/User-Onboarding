f0190cfd0d80199f5e9d90ff871ffde36877c9bd
Ryan Mark <rymark907@gmail.com>
2021-09-16 12:34:53 -0700

    Got name, email, password, and checkbox tests running and passing

--- user-onboarding/cypress/integration/1-getting-started/form_test.spec.js
+++ user-onboarding/cypress/integration/1-getting-started/form_test.spec.js
@@ -8,10 +8,28 @@ describe('Checking that cypress can navigate to my site and run tests', () => {
     const emailInput = () => cy.get('input[name=email]');
     const passwordInput = () => cy.get('input[name=password]');
     const termsInput = () => cy.get('input[name=terms]');
+    
 
     it('grabbing the name input and typing the name into it', () => {
         textInput().should('exist');
         textInput().type('Ryan');
-        textInput().should('exist').contains('Ryan');
+        textInput().should('have.value', 'Ryan');
+    })
+
+    it('grabbing the email input and typing an email into it', () => {
+        emailInput().should('exist');
+        emailInput().type('fakeemail@email.com');
+        emailInput().should('have.value', 'fakeemail@email.com');
+    })
+    
+    it('grabbing the password input and typing a password into it', () => {
+        passwordInput().should('exist');
+        passwordInput().type('somepassword');
+        passwordInput().should('have.value', 'somepassword');
+    })
+
+    it('checking if the terms of service can be checked', () => {
+        termsInput().should('exist');
+        termsInput().click();
     })
 })
