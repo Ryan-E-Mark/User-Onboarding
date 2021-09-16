e50ddf506d4c7b37f76081e8c7595c2404cbd38d
Ryan Mark <rymark907@gmail.com>
2021-09-16 12:38:04 -0700

    submitting the form test passes

--- user-onboarding/cypress/integration/1-getting-started/form_test.spec.js
+++ user-onboarding/cypress/integration/1-getting-started/form_test.spec.js
@@ -8,6 +8,7 @@ describe('Checking that cypress can navigate to my site and run tests', () => {
     const emailInput = () => cy.get('input[name=email]');
     const passwordInput = () => cy.get('input[name=password]');
     const termsInput = () => cy.get('input[name=terms]');
+    const submitBtn = () => cy.get('button');
     
 
     it('grabbing the name input and typing the name into it', () => {
@@ -31,5 +32,14 @@ describe('Checking that cypress can navigate to my site and run tests', () => {
     it('checking if the terms of service can be checked', () => {
         termsInput().should('exist');
         termsInput().click();
+    })
+
+    it('checking to see if I can submit the form', () => {
+        textInput().type('Ryan');
+        emailInput().type('anemail@email.com');
+        passwordInput().type('apassword');
+        submitBtn().should('exist');
+        termsInput().click();
+        submitBtn().click();
     })
 })
